b163cdbdc9ef3496e25dae3af8f558381702f5c8
Birna Birna <birnabirna@Birnas-MBP-2.tskoli.is>
2018-04-05 13:42:01 +0000

    key drasl

--- src/App.js
+++ src/App.js
@@ -63,10 +63,12 @@ class App extends Component {
           isMarkerShown
           // googleMapURL="https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places"
           // googleMapURL="https://maps.googleapis.com/maps/api/js?key=AIzaSyATX8t28De8FbC9I3mqBxXSHlCvJJgbx_k"
-          googleMapURL="https://maps.googleapis.com/maps/api/js?libraries=places&key=AIzaSyATX8t28De8FbC9I3mqBxXSHlCvJJgbx_k"
+          googleMapURL="https://maps.googleapis.com/maps/api/js?libraries=places&key=AIzaSyDqoBl3H83O5bhKf-36jyQqi2Nlqo3t21A"
           // googleMapURL="https://maps.googleapis.com/maps/api/js?key=AIzaSyATX8t28De8FbC9I3mqBxXSHlCvJJgbx_k&callback=initMap"
           // 
           // AIzaSyATX8t28De8FbC9I3mqBxXSHlCvJJgbx_k
+          // AIzaSyDqoBl3H83O5bhKf-36jyQqi2Nlqo3t21A
+
           loadingElement={<div style={{ height: `100%` }} />}
           containerElement={<div style={{ height: `400px` }} />}
           mapElement={<div style={{ height: `100%` }} />} />
